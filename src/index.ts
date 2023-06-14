8ca381e6ddf2a2e2034d6d77758703d208a8d665
Tsapov-Maxim <tsapov552@gmail.com>
2023-06-14 11:57:58 +0700

    Test

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ AppDataSource.initialize()
     app.use(errorHandler);
 
     // start express server
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
 
     console.log(
       "Express server has started on port 3000. Open http://localhost:3000/users to see results"
